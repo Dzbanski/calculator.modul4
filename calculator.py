1ab1ed22bdaf5a39af631787cc6f0df0df88c662
Jacek Dzbański <jacek17123@gmail.com>
2026-01-13 05:05:53 +0100

    I added a loop and a tuple to add as many numbers as the user wants

--- calculator.py
+++ calculator.py
@@ -3,15 +3,23 @@ import sys
 
 logging.basicConfig(level=logging.DEBUG)
 
-try:
+numbers = []
+try: 
     choice = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie"))
-    a = float(input("Podaj składnik 1."))
-    b = float(input("Podaj składnik 2."))
+    if choice in [1,3]:
+        many = int(input("Na ilu liczbach chcesz wykonać obliczenia? :"))
+        for i in range(many):
+            next_number = float(input("Podaj składnik %s" % (i+1)))
+            numbers.append(next_number)
+    else:     
+        a = float(input("Podaj składnik 1."))
+        b  = float(input("Podaj składnik 2."))
 except ValueError:
     logging.warning("Nie podałeś liczby !")
     sys.exit(0)
 
-def operation(choice, a,b):
+
+def operation(choice, *args):
     """
     Function operation takes three positional parameters. The user's selection causes the main information available in the search results to be displayed
     
@@ -19,9 +27,9 @@ def operation(choice, a,b):
     :param a: number 1
     :param b: number 2
     """
-    if choice == 1:
-        logging.info("Dodaję : %s i %s", a,b)
-        return a+b
+    if choice == 1: 
+        logging.info("Dodaję : %s", args)
+        return sum(args)
     elif choice == 2:
         logging.info("Odejmuję : %s i %s", a,b)
         return a - b
@@ -37,6 +45,6 @@ def operation(choice, a,b):
             return a / b
 
 
-wynik = operation(choice,a,b)
+wynik = operation(choice,*numbers)
 if wynik is not None:
     print("Wynik to :", wynik)
