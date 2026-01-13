8dfed51861372bb57ef77ae46c564e25feae5d31
Jacek Dzbański <jacek17123@gmail.com>
2026-01-13 04:18:18 +0100

    Add function operation which return calculations and informations

---
+++ calculator.py
@@ -0,0 +1,34 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+choice = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie"))
+a = float(input("Podaj składnik 1."))
+b = float(input("Podaj składnik 2."))
+
+
+
+def operation(choice, a,b):
+   
+    if choice == 1:
+        logging.info("Dodaję : %s i %s", a,b)
+        return a+b
+    elif choice == 2:
+        logging.info("Odejmuję : %s i %s", a,b)
+        return a - b
+    elif choice == 3:
+        logging.info("Mnożę : %s i %s", a,b)
+        return a * b
+    elif choice == 4:
+        if b == 0:
+            logging.warning("Nie dzielimy przez 0")
+            return None
+        else:
+            logging.info("Dzielę : %s i %s", a,b)
+            return a / b
+
+
+wynik = operation(choice,a,b)
+if wynik is not None:
+    print("Wynik to :", wynik)
