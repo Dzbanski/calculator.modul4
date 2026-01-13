923a17f8aef1e735d596b54184bfee98d1dc2a0a
Jacek Dzbański <jacek17123@gmail.com>
2026-01-13 04:21:32 +0100

    Add documentation below function

--- calculator.py
+++ calculator.py
@@ -12,7 +12,13 @@ except ValueError:
     sys.exit(0)
 
 def operation(choice, a,b):
-   
+    """
+    Function operation takes three positional parameters. The user's selection causes the main information available in the search results to be displayed
+    
+    :param choice: User's can choose addition(1), subtraction(2), multiplication(3), division(4)
+    :param a: number 1
+    :param b: number 2
+    """
     if choice == 1:
         logging.info("Dodaję : %s i %s", a,b)
         return a+b
